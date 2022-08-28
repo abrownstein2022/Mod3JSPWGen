6be1734764755c2f15d0ab821d3b1d7760a0c684
Alexis Brownstein <alexis@drdatabase.com>
2022-08-28 12:04:06 -0400

    fixed issues in pw len validation and hooked up logic to existing button code given in starter code

--- Develop/script.js
+++ Develop/script.js
@@ -1,41 +1,36 @@
 // Challenge 3 JavaScript Password Generator
-//Alexis Brownstein 8/28/22
+//Alexis Brownstein 8/28/22  NOTE to self: JS is case-sensitive.  Watch how you spell variables, functions, etc.
 
 let pwText = "yourpassword"; //initialize for test purposes. Will clear out when go live.
 let isOK = false; //only call password generator if all criteria ok and set isOK to true
 
 var generatePassword = function() {
-  // Ask user to enter password length
-   let pwLen = window.prompt("Please enter desired password length");
+// Ask user to enter password length
+let pwLen = window.prompt("Please enter desired password length");
   
-   // If user pressed Cancel, immediately end function
-   if (!pwLen) {
-     console.log("You pressed cancel.  Exiting function.")
-     return;
-   }
-    console.log(pwLen);
+// If user pressed Cancel, immediately end function
+if (!pwLen) {
+   window.alert("You pressed cancel.  Exiting function.")
+   return;
+ }
+   //window.alert(pwLen);
 
-   //validate pwLen between 8 and 128 inclusive
-   if (pwLen < 8 || pwLen > 128){
-    console.log("Error: Password length must be between 8 and 128.  Please try again.")
-    return;
-   }
+//validate pwLen between 8 and 128 inclusive
+ if (pwLen < 8 || pwLen > 128){
+   window.alert("Error: Password length must be between 8 and 128.  Please try again.")
+ }
 
-  // Passed all validations so display generated password
-  window.alert(
-    "Here's your password: " + pwText + "\nThank you for using my password generator!" 
-  );
+// Passed all validations so display generated password
+ if (isOK){ 
+  writePassword() + "\nThank you for using my password generator!"; 
+ } else {
+  return;
+ }
 
 };  //end to var genPassword = function() at top of code
 
-// Run the password generator (entire code above)
-if (isOK){ 
-  writePassword();
-}
-
 generatePassword();
 //end Alexis code
-
 
 //below is included starter code
 // Get references to the #generate element
