857e5f8fc8f0051749ab92cd4bc014423175ee77
Alexis Brownstein <alexis@drdatabase.com>
2022-08-28 11:50:32 -0400

    added initial logic for password length input and validation

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,43 @@
-// Assignment code here
+// Challenge 3 JavaScript Password Generator
+//Alexis Brownstein 8/28/22
+
+let pwText = "yourpassword"; //initialize for test purposes. Will clear out when go live.
+let isOK = false; //only call password generator if all criteria ok and set isOK to true
+
+var generatePassword = function() {
+  // Ask user to enter password length
+   let pwLen = window.prompt("Please enter desired password length");
+  
+   // If user pressed Cancel, immediately end function
+   if (!pwLen) {
+     console.log("You pressed cancel.  Exiting function.")
+     return;
+   }
+    console.log(pwLen);
+
+   //validate pwLen between 8 and 128 inclusive
+   if (pwLen < 8 || pwLen > 128){
+    console.log("Error: Password length must be between 8 and 128.  Please try again.")
+    return;
+   }
+
+  // Passed all validations so display generated password
+  window.alert(
+    "Here's your password: " + pwText + "\nThank you for using my password generator!" 
+  );
+
+};  //end to var genPassword = function() at top of code
+
+// Run the password generator (entire code above)
+if (isOK){ 
+  writePassword();
+}
+
+generatePassword();
+//end Alexis code
 
 
+//below is included starter code
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
